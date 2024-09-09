e35e643c26c1eaacb86e4aa130bc7998e39b90a8
StrangeJZ <juanjosezavalajr@gmail.com>
2024-09-09 12:04:22 -0400

    Branching

---
+++ main.py
@@ -0,0 +1,25 @@
+hours_used = 1500
+if(hours_used < 1000):
+    print( "Amount owed is $", hours_used * 0.07633)
+elif(hours_used > 1000):
+    print("Amount owed is $", hours_used * 0.09259 - 16.26)
+
+hours_used_2 = 764
+if(hours_used_2 < 1000):
+    print("Amount owed is $", hours_used_2 * 0.07633)
+elif(hours_used_2 > 1000):
+    print("Amount owed is $", hours_used_2 * 0.09259 - 16.26)
+
+hours_used_3 = 1215
+if(hours_used_3 < 1000):
+    print("Amount owed is $", hours_used_3 * 0.07633)
+elif(hours_used_3 > 1000):
+    print("Amount owed is $", hours_used_3 * 0.09259 - 16.26)
+
+hours_used_4 = 812
+if(hours_used_4 < 1000):
+    print("Amount owed is $", hours_used_4 * 0.07633)
+elif(hours_used_4 > 1000):
+    print("Amount owed is $", hours_used_4 * 0.09259 - 16.26)
+
+print("Juan Zavala")
